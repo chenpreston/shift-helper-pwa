d53a1a927ced75836741e6da89cb10bbcdcf50aa
IOHX180 <chen.preston@gmail.com>
2025-03-18 10:36:27 +1300

    v0.2.4

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = "driver-helper-cache-v0.2.3"; // 更新缓存时修改版本号                             
+const CACHE_NAME = "driver-helper-cache-v0.2.4"; // 更新缓存时修改版本号                                  
 const urlsToCache = [
   "/",
   "./index.html",
   "./about.html",
+  "./manifest.json",
   //  "./css/style.css",
   //  "./js/script.js",
   //  "./data/shifts.csv",
