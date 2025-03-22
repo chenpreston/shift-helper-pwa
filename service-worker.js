736265033e0715c6ddd28c2aa6639ab6915a42ea
Preston Chen <chen.preston@gmail.com>
2025-03-23 02:56:46 +1300

    v0.3.0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "driver-helper-cache-v0.2.9"; // 更新缓存时修改版本号                                     
+const CACHE_NAME = "driver-helper-cache-v0.3.0"; // 更新缓存时修改版本号                                      
 const urlsToCache = [
   "/",
   "./index.html",
